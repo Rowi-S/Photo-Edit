b99ca5ff4f467573e7fa9e6f2d86d1ed8d68f949
Rowi-S <hgnisiwor@gmail.com>
2024-10-11 16:53:29 +0200

    Feature black white + save new img

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	"image/png"
 	"os"
 )
@@ -23,6 +24,14 @@ func main() {
 	}
 
 	fmt.Println("Image dimensions:", img.Bounds())
+
+	bwImg := convertToBlackAndWhite(img)
+
+	if err := saveImage(bwImg, "bw_go_mascot.png"); err != nil {
+		fmt.Println("Failed to save image", err)
+		return
+	}
+
 }
 
 func greetingScreen() {
@@ -38,4 +47,33 @@ func openingFile() (*os.File, error) {
 func decodingFile(file *os.File) (image.Image, error) {
 	img, err := png.Decode(file)
 	return img, err
+}
+
+func convertToBlackAndWhite(img image.Image) image.Image {
+	bounds := img.Bounds()
+	bwImg := image.NewGray(bounds)
+
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			originalColor := img.At(x, y)
+			grayColor := color.GrayModel.Convert(originalColor)
+			bwImg.Set(x, y, grayColor)
+		}
+	}
+	return bwImg
+}
+
+func saveImage(img image.Image, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("could not create file: %w ", err)
+	}
+	defer file.Close()
+
+	if err := png.Encode(file, img); err != nil {
+		return fmt.Errorf("could not encode image: %w", err)
+	}
+
+	fmt.Println("Image saved successfully as:", filename)
+	return nil
 }
